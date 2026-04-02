944d101a3b78f487d67ad40b0a3013be28a3725b
Tyron Fajardo <tyronjf44@gmail.com>
2026-04-02 12:25:46 -0400

    adding and fixing testing script

--- Assets/Scripts/Lab 8 Scripts/PlayerTests.cs
+++ Assets/Scripts/Lab 8 Scripts/PlayerTests.cs
@@ -1,16 +1,38 @@
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 
-public class PlayerTests : MonoBehaviour
+public class PlayerTests
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameObject player;
+
+    [SetUp]
+    public void Setup()
     {
-        
+        player = new GameObject("Player");
+
+        Rigidbody2D rb = player.AddComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.gravityScale = 0f;
+
+        player.AddComponent<BoxCollider2D>();
+        player.AddComponent<Player2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    [Test]
+    public void Player_HasRequiredComponents()
     {
-        
+        Assert.IsNotNull(player.GetComponent<Rigidbody2D>());
+        Assert.IsNotNull(player.GetComponent<Collider2D>());
+        Assert.IsNotNull(player.GetComponent<Player2>());
+    }
+
+    [Test]
+    public void Rigidbody_IsConfiguredCorrectly()
+    {
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+
+        Assert.AreEqual(RigidbodyType2D.Dynamic, rb.bodyType);
+        Assert.AreEqual(0, rb.gravityScale);
     }
 }
